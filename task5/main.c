7b1ab02fe942b99013773424dd6b028309cfb9b3
vidanm <vidan.tauriff@gmail.com>
2024-04-10 17:02:19 +0200

    task 5 done

--- task5/main.c
+++ task5/main.c
@@ -3,28 +3,53 @@
  */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
+#include <linux/usb.h>
+#include <linux/hid.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
-static char* whom = "world";
-static int howmany = 1;
-module_param(howmany,int,S_IRUGO);
-module_param(whom,charp,S_IRUGO);
+static int eud_probe(struct usb_interface *interface,const struct usb_device_id *id){
+	printk("Keyboard connected\n");
+	return 0;
+}
+
+static void eud_disconnect(struct usb_interface *interface){
+	printk("Keyboard disconnected\n");
+}
+
+static struct usb_device_id eud_table [] = {
+        { USB_INTERFACE_INFO
+	 (USB_INTERFACE_CLASS_HID, USB_INTERFACE_SUBCLASS_BOOT,
+	  USB_INTERFACE_PROTOCOL_KEYBOARD) },
+        { }                      /* Terminating entry Parce qu'une entrée se termine à Null*/
+};
+
+/* Permets de définir à quel groupe de device USB ce driver s'addresse */
+MODULE_DEVICE_TABLE (usb, eud_table);
+
+static struct usb_driver keyboard_driver = {
+	.name = "Eudyptula_task5",
+	.probe = eud_probe,
+	.disconnect = eud_disconnect,
+	.id_table = eud_table
+};
 
 static int hello_init(void)
 {	
-	int i = 0;
-	for (i = 0;i<howmany;i++)
-		printk(KERN_ALERT "%s",whom);
+	int result;
+	result = usb_register(&keyboard_driver);
+	if (result < 0) {
+		pr_err("usb_register failed for the %s driver. Error number %d\n",
+                       keyboard_driver.name, result);
+		return -1;
+	}
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	usb_deregister(&keyboard_driver);
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-

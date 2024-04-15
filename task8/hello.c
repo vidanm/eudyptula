4b0bebd6f2bc072ada989b6fbf93eb5e785cf7e7
vidanm <vidan.tauriff@gmail.com>
2024-04-15 19:24:27 +0200

    task 8 done

--- task8/hello.c
+++ task8/hello.c
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/miscdevice.h>
 #include <linux/jiffies.h>
 #include <linux/debugfs.h>
+#include <linux/semaphore.h>
 #define MY_USER_ID "11121998"
 #define USER_ID_LENGTH 9 //\0
 
 MODULE_LICENSE("Dual BSD/GPL");
+static DEFINE_SEMAPHORE(foo_sem,1); //DECLARE_MUTEX HAS BEEN RENAMED TO DEFINE_SEMAPHORE
 
 /* fid file fops */
 static ssize_t fid_read(struct file *filp, char __user *buf, size_t len, loff_t *f_pos) {
@@ -33,19 +34,16 @@ static ssize_t fid_write(struct file *file, const char __user *buf, size_t len, 
 	
 	if (len != USER_ID_LENGTH || ret || strncmp(user_id,input,USER_ID_LENGTH-1))
 		return -EINVAL;
-	else {
-		pr_info("Data written to eudyptula/id is user id\n");
+	else
 		return len;
-	}
+
 }
 
 static int fid_open(struct inode *inode, struct file *file){
-	pr_info("eudyptula/id open\n");
 	return 0;
 }
 
 static int fid_close(struct inode *inode, struct file *file){
-	pr_info("eudyptula/id close\n");
 	return 0;
 }
 
@@ -60,6 +58,69 @@ static const struct file_operations fid_fops = {
 
 /* end of fid file ops */
 
+/* start of foo file ops */
+
+static char foo_data[PAGE_SIZE];
+static int foo_len;
+
+static ssize_t foo_read(struct file *filp, char __user *buf, size_t len, loff_t *f_pos) {
+	int ret;
+
+	if (*f_pos != 0)
+		return 0;
+
+	if (down_interruptible(&foo_sem))
+		return -EINVAL;
+
+	ret = copy_to_user(buf,foo_data,foo_len);
+	up(&foo_sem);
+
+	if (ret)
+		return -EINVAL;
+
+	*f_pos += len;
+	return len;
+
+}
+
+static ssize_t foo_write(struct file *file, const char __user *buf, size_t len, loff_t  *f_pos) {
+	int ret;
+
+	if (down_interruptible(&foo_sem))
+		goto fail;
+
+	ret = copy_from_user(foo_data,buf,len);
+	up(&foo_sem);
+
+	if (ret)
+		goto fail;
+
+	foo_len = len;
+	return len;
+
+	fail: foo_len = 0;
+	      return -EINVAL;
+}
+
+static int foo_open(struct inode *inode, struct file *file){
+	return 0;
+}
+
+static int foo_close(struct inode *inode, struct file *file){
+	return 0;
+}
+
+static const struct file_operations foo_fops = {
+	.owner = THIS_MODULE,
+	.write = foo_write,
+	.read = foo_read,
+	.open = foo_open,
+	.release = foo_close,
+	.llseek = no_llseek,
+};
+
+/* end of foo file ops */
+
 static struct dentry *eud_dentry;
 static int hello_init(void)
 {
@@ -69,23 +130,24 @@ static int hello_init(void)
 		return -ENODEV;
 	}
 
-	
 	struct dentry* eud_id = debugfs_create_file("id",0666,eud_dentry,NULL,&fid_fops);
-	debugfs_create_u32("jiffies",0444,eud_dentry,(u32*)&jiffies);
+	debugfs_create_ulong("jiffies",0444,eud_dentry,(unsigned long *)&jiffies); 
+	struct dentry* eud_foo = debugfs_create_file("foo",0744,eud_dentry,NULL,&foo_fops);
 
-	if (!eud_id)
+	if (!eud_id || !eud_foo) 
 	{
+		debugfs_remove_recursive(eud_dentry);
 		pr_alert("Files could not be created\n");
 		return -ENODEV;
 	}
 
-	pr_alert("eudyptula task 8 module : added\n");
+	pr_alert("Eudyptula module, task 8 : added\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	pr_alert("eudyptula task 8 module : removed\n");
+	pr_alert("Eudyptula module, task 8 : removed\n");
 	debugfs_remove_recursive(eud_dentry);
 }
 
---
+++ task8/how.md
@@ -0,0 +1,5 @@
+# Usefull doc
+https://www.kernel.org/doc/Documentation/filesystems/debugfs.txt
+https://litux.nl/mirror/kerneldevelopment/0672327201/ch10lev1sec3.html
+https://www.infradead.org/~mchehab/kernel\_docs/filesystems/debugfs.html
+Linux Device Drivers Book - Chapter 05
